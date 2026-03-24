8105b1af76d3146cb5d67d021398420d087c38eb
Ric@rdo López <ricardo.softwaredeveloper8@gmail.com>
2026-03-24 17:11:50 -0500

    Update streamlit_app.py
    se incluye variable de conexion para snlwflake y se activa la el metodo sesion con la libreria st "streamlit"

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,9 @@ st.write(
 # st.write("your favorite fruit is: ", option)
 
 
-session = get_active_session()
+#session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 
