80b5b5098fc20055e238fd154116b75e8be392f4
Ric@rdo López <ricardo.softwaredeveloper8@gmail.com>
2026-03-25 14:25:26 +0000

    se incluye ciclo for

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,8 +41,9 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-
-    # st.write(ingredients_string)
+        st.subheader(fruit_chosen + 'Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+        af_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
