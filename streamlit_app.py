4e340aa6ce638602f3652907af785e3eda24f6ec
Ric@rdo López <ricardo.softwaredeveloper8@gmail.com>
2026-03-25 15:59:28 +0000

    se comentarea lineas 33 y 34 de pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,8 +30,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 #st.stop()
 
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+#st.dataframe(pd_df)
+#st.stop()
 
 
 ingredients_list = st.multiselect(
@@ -48,7 +48,7 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-        
+
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
 
